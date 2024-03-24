51e4486c6068193ce550fffba1ae186fb39fe67e
SILATAKU2 <katerinataku@gmail.com>
2024-03-25 00:16:33 +0200

    completed homepage

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favorite food here</h2>
+            <p>Choose well cooked meals from a diverse menu with carefully picked ingredients, made with love.</p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
+
+const Navbar = ({setShowLogin}) => {
+
+    const [menu, setMenu] = useState("home");
+
+    return (
+        <div className='navbar'>
+            <img src={assets.logo} alt="" className="logo" />
+            <ul className="navbar-menu">
+                <li>
+                    <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
+                </li>
+                <li>
+                    <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
+                </li>
+                <li>
+                    <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}> Mobile-app</a>
+                </li>
+                <li>
+                    <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}> Contact us</a>
+                </li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                    <button onClick={()=> setShowLogin(true)}>Sign in</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar
+
